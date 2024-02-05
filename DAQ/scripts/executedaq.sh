31992bae9b3cba0821c4e027ade5f18a373474de
cup-software2018 <158539646+cup-software2018@users.noreply.github.com>
2024-02-05 10:56:21 +0900

    Update executedaq.sh for new soft scheme

--- DAQ/scripts/executedaq.sh
+++ DAQ/scripts/executedaq.sh
@@ -2,7 +2,7 @@
 
 export MALLOC_CHECK_=0
 
-OPTS=`getopt -o r:o:n:dmt --long onldaqdir:,rawdatadir:,version: -n 'executedaq.sh' -- "$@"`
+OPTS=`getopt -o r:o:n:dmt --long onldaqdir:,rawdatadir: -n 'executedaq.sh' -- "$@"`
 
 if [ $? != 0 ]; then
     echo "Terminating..." >&2; exit 1;
@@ -13,7 +13,6 @@ eval set -- "$OPTS"
 RUNNUM=
 ONLDAQDIR=
 RAWDATADIR=
-VERSION=
 EXE=
 ADCOPT=
 DAQID=
@@ -37,8 +36,6 @@ while true; do
         shift 2;;
     --rawdatadir) RAWDATADIR="$2"
         shift 2;;
-    --version) VERSION="$2"
-        shift 2;;
     --) shift;
         break ;;
     *) break ;;
@@ -57,14 +54,12 @@ fi
 
 if [ "$VERSION" ]
 then
-    . /home/cupsoft/cup_setup.sh "$VERSION"
-    export LD_LIBRARY_PATH=${ONLDAQ_DIR}/shlib/${BFARCH}:$LD_LIBRARY_PATH
+    # plz setting modules (like root, ...), here ...!!!
+    export LD_LIBRARY_PATH=${ONLDAQ_DIR}/lib:$LD_LIBRARY_PATH
 fi
 
 RUNNUMSTR=`printf "%06d" $RUNNUM`
 
-${ONLDAQ_DIR}/bin/${BFARCH}/$EXE $DAQOPT > ${RAWDATA_DIR}/LOG/${DAQNAME}_${RUNNUMSTR}.log 2>&1 &
-
-#CMD="${ONLDAQ_DIR}/bin/${BFARCH}/$EXE $DAQOPT > ${RAWDATA_DIR}/LOG/${DAQNAME}_${RUNNUMSTR}.log 2>&1 &"
+${ONLDAQ_DIR}/bin/$EXE $DAQOPT > ${RAWDATA_DIR}/LOG/${DAQNAME}_${RUNNUMSTR}.log 2>&1 &
 
 exit
