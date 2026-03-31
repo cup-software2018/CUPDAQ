4c32e55a6790fffb5206b5a82dfe7d488ac7a90e
Jaison Lee <jsahnlee@gmail.com>
2026-03-31 15:19:11 +0900

    remove AMORE stuff from HDF5Utils

--- HDF5Utils/HDF5Utils/EDM.hh
+++ HDF5Utils/HDF5Utils/EDM.hh
@@ -9,7 +9,6 @@
 #include "hdf5.h"
 
 constexpr int kH5FADCNDPMAX = 16384;   // 64 us for FADC
-constexpr int kH5AMORENDPMAX = 500000; // 5 s for AMOREADC
 
 struct SubRun_t {
   std::uint32_t subrun;
@@ -73,36 +72,6 @@ struct AChannel_t {
       time(0)
   {
   }
-
-  static hid_t BuildType();
-  hsize_t GetSize() const noexcept;
-};
-
-struct Crystal_t {
-  std::uint16_t id;
-  std::uint16_t phonon[kH5AMORENDPMAX];
-  std::uint16_t photon[kH5AMORENDPMAX];
-  std::uint64_t ttime;
-
-  Crystal_t() noexcept
-    : id(0),
-      ttime(0)
-  {
-    std::memset(phonon, 0, sizeof(phonon));
-    std::memset(photon, 0, sizeof(photon));
-  }
-
-  static hid_t BuildType();
-  hsize_t GetSize() const noexcept;
-
-  void SetPhonon(const std::uint16_t * wave, int ndp) noexcept;
-  void SetPhoton(const std::uint16_t * wave, int ndp) noexcept;
-  void SetWaveforms(const std::uint16_t * pn, const std::uint16_t * pt, int ndp) noexcept;
-};
-
-struct CrystalHeader_t {
-  std::uint16_t id;
-  std::uint64_t ttime;
 
   static hid_t BuildType();
   hsize_t GetSize() const noexcept;
@@ -197,46 +166,3 @@ inline hid_t AChannel_t::BuildType()
 }
 
 inline hsize_t AChannel_t::GetSize() const noexcept { return sizeof(AChannel_t); }
-
-inline hid_t Crystal_t::BuildType()
-{
-  hid_t type = H5Tcreate(H5T_COMPOUND, sizeof(Crystal_t));
-  H5Tinsert(type, "id", HOFFSET(Crystal_t, id), H5T_STD_U16LE);
-
-  hsize_t dim[1] = {kH5AMORENDPMAX};
-  hid_t arrtype = H5Tarray_create2(H5T_STD_U16LE, 1, dim);
-  H5Tinsert(type, "phonon", HOFFSET(Crystal_t, phonon), arrtype);
-  H5Tinsert(type, "photon", HOFFSET(Crystal_t, photon), arrtype);
-  H5Tinsert(type, "ttime", HOFFSET(Crystal_t, ttime), H5T_STD_U64LE);
-
-  return type;
-}
-
-inline void Crystal_t::SetPhonon(const std::uint16_t * wave, int ndp) noexcept
-{
-  std::memcpy(phonon, wave, static_cast<std::size_t>(ndp) * sizeof(std::uint16_t));
-}
-
-inline void Crystal_t::SetPhoton(const std::uint16_t * wave, int ndp) noexcept
-{
-  std::memcpy(photon, wave, static_cast<std::size_t>(ndp) * sizeof(std::uint16_t));
-}
-
-inline void Crystal_t::SetWaveforms(const std::uint16_t * pn, const std::uint16_t * pt,
-                                    int ndp) noexcept
-{
-  SetPhonon(pn, ndp);
-  SetPhoton(pt, ndp);
-}
-
-inline hsize_t Crystal_t::GetSize() const noexcept { return sizeof(Crystal_t); }
-
-inline hid_t CrystalHeader_t::BuildType()
-{
-  hid_t type = H5Tcreate(H5T_COMPOUND, sizeof(CrystalHeader_t));
-  H5Tinsert(type, "id", HOFFSET(CrystalHeader_t, id), H5T_STD_U16LE);
-  H5Tinsert(type, "ttime", HOFFSET(CrystalHeader_t, ttime), H5T_STD_U64LE);
-  return type;
-}
-
-inline hsize_t CrystalHeader_t::GetSize() const noexcept { return sizeof(CrystalHeader_t); }
--- HDF5Utils/HDF5Utils/LinkDef.h
+++ HDF5Utils/HDF5Utils/LinkDef.h
@@ -7,7 +7,6 @@
 #pragma link C++ class  AbsH5Event;
 #pragma link C++ class  H5FADCEvent;
 #pragma link C++ class  H5SADCEvent;
-#pragma link C++ class  H5AMOREEvent;
 #pragma link C++ class  H5DataWriter;
 #pragma link C++ class  H5ChainFile;
 #pragma link C++ class  H5DataReader;
